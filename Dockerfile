0e4d809a93634eaf59d3f98ab7cec45278f3474e
Fernando Penna <futured@gmail.com>
2025-02-22 19:14:16 -0300

    fix: removing deprecated serde_yaml and updating images

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust:1.79-bullseye AS builder
+FROM rust:1.85-bullseye AS builder
 
 WORKDIR /usr/src/cron-cloudflare
 
--- src/services/config_reader.rs
+++ src/services/config_reader.rs
@@ -6,7 +6,7 @@ use crate::models::config::Config;
 pub fn get_config() -> Result<Config, Box<dyn Error>> {
     let config_file_path = env::var("CONFIG_FILE_PATH").expect("$CONFIG_FILE_PATH is not set");
     let content_yaml = fs::read_to_string(config_file_path)?;
-    let config = serde_yaml::from_str::<Config>(&content_yaml)?;
+    let config = serde_yml::from_str::<Config>(&content_yaml)?;
 
     Ok(config)
 }
